625dd2f537fbf85fd4143a28a3274732f59446da
lemon-juicee <isaacdoroshow@gmail.com>
2025-01-10 14:47:49 -0800

    Separate Generic_Mask_Filter into its own script

--- cleaned_ship_graphing.py
+++ cleaned_ship_graphing.py
@@ -7,76 +7,9 @@ import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 import datetime
 import math
+from gmc import Generic_Mask_Filter
 
-# Generic_Mask_Filter() written by Diran Jimenez
-def Generic_Mask_Filter(file_path, MMSI=False, BaseDateTime=False, LAT=False, LON=False, SOG=False, COG=False,
-                        Heading=False, VesselName = False, IMO = False, CallSign = False, VesselType = False,
-                        Status = False, Length = False, Width = False, Draft = False, Cargo = False, TransceiverClass = False):
-    """
-    Parameters
-    ----------
-    file_path : string
-        This string should point to a CSV File of AIS Broadcast Data you wish to filter.
-    ALL OTHER PARAMETERS: List
-       
-        Each parameter corresponds to a column of AIS Broadcast Data
-       
-        All values to be checked must be passed as a list, even if it is a
-            single value. This allows the function to check each column for
-            multiple values
-       
-        The data assumes the "and" condition between all columns, and the
-            "or" condition between values in a column.
-           
-        EX:
-            Generic_Mask_Filter(MMSI = [12345], Status = [2]) will return
-            broadcasts that have MMSI = 12345 & Status = 2
-       
-        EX:
-            Generic_Mask_Filter(MMSI = [12345, 99999]) will return broadcasts
-            that have MMSI = 12345 || MMSI = 99999
-                Note that the "or" condition is denoted with "||" by pandas and numpy
-       
-    Returns
-    -------
-    df : DataFrame
-        A DataFrame that has data which satisfies all criterion passed as input
-        conditions.
-
-    """
-    # locals() creates a dictionary containing all local variables
-    conditions = locals()
-    del conditions["file_path"]
-        # The only variable not tied to a condition is the file_path
-
-    df = pd.read_csv(file_path, sep=',', header=0, dtype={"Heading": "Int64",
-                                                               "VesselName": str,
-                                                               "IMO":str,
-                                                               "MMSI":str, #Technically this should be an integer, but some files accidentally insert an alphanumeric character, causing a ValueError
-                                                               "LAT":np.float64,
-                                                               "LON":np.float64,
-                                                               "SOG":np.float64,
-                                                               "Heading":"Int64",
-                                                               "COG":np.float64,
-                                                               "IMO":str,
-                                                               "CallSign":str,
-                                                               "VesselType":"Int64",
-                                                               "Status":"Int64",
-                                                               "Length":"Int64",
-                                                               "Width":"Int64",
-                                                               "Cargo":"Int64",
-                                                               "Draft":np.float64,
-                                                               "TransceiverClass":str,
-                                                               "TranscieverClass":str}, on_bad_lines="skip")
-   
-    # Apply each condition to the DataFrame
-    for column, requirements in conditions.items():
-        if requirements:
-            df = df.loc[df[column].isin(requirements)]
-
-    return df
-
-# Code from here written by Lemon Doroshow
+# Code written by Lemon Doroshow
 def closest(lst, K):
     """
     closest() finds the closest value in a list to the given value
