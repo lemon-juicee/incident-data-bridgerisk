ca326b7d8db0a794be01442ff01fbd5af07a7898
lemon-juicee <isaacdoroshow@gmail.com>
2025-01-13 18:45:35 -0800

    Update comments across param_collection()

--- bridge_pass_collection.py
+++ bridge_pass_collection.py
@@ -60,6 +60,7 @@ def bridge_reader(path):
     passes_paired = pandas dataframe containing the MMSI, date, and times before and after each pass
         type = pandas.DataFrame
     """
+
     # Import csv
     df = pd.read_csv(path, sep=',', header=0, on_bad_lines="skip")
 
@@ -83,11 +84,24 @@ def bridge_reader(path):
     return passes_paired
 
 def param_collection(path, param):
-    # WiP docstring: the goal of param_collection() is to take a dataframe compiled by bridge_reader() 
-    # and collect the COGs 5 miles upstream and downstream from the time of pass
+    """
+    param_collection() takes a csv file (formatted the same way as bridge_reader()'s input) and collects a certain parameter for every bridge pass in the file and ~5 miles up and downstream
+    Parameters:
+    path = the bridge csv file's path - MUST BE a csv file pre-compiled from our GitHub repo
+        type = str
+    param = the parameter to collect, must be in ["LAT", "LON", "SOG", "Heading", "COG", "IMO", "Status", "Draft", "Angle Difference"]
+        type = str 
+    Returns:
+    collection = the collection of the parameters in the ~10 mile range up and downstream from the bridge pass
+        type = list
+    """
+    # Build bridge dataframe 
     bridge_df = bridge_reader(path)
     collection = []
-    for passing in bridge_df.itertuples():
+
+    for passing in bridge_df.itertuples(): # Iterates through the dataframe by row (for each pass)
+
+        # Import data by pass and filter depending on parameter
         data = Generic_Mask_Filter('data/AIS_' + passing.date + '.csv', MMSI = [str(passing.MMSI)])
         if param == 'COG':
             data = data[data['COG'] != 360.0]
@@ -102,42 +116,50 @@ def param_collection(path, param):
         data = data.sort_values(by='BaseDateTime')
         data.reset_index(drop=True, inplace=True)
 
-        ind = 0 # Debugging
+        # Upstream loop
+        ind = 0 # For debugging
         index_before = data.index[data['BaseDateTime'] == passing.time_before][0]
         distance_before = 0
         coordinate_prior = (data['LAT'].tolist()[index_before], data['LON'].tolist()[index_before])
-        while distance_before <= 5:
+
+        while distance_before <= 5: # 5 miles upstream
+            # Coordinate variables used to measure the distance between each datapoint as we iterate
             coordinate = (data['LAT'].tolist()[index_before], data['LON'].tolist()[index_before])
             coordinate_prior = (data['LAT'].tolist()[index_before + 1], data['LON'].tolist()[index_before + 1])
             if param != 'Angle Difference':
                 collection.append(data[param].tolist()[index_before])
             elif param == 'Angle Difference':
+                # Calculates angle difference according to rules set out in true_difference() function definition
                 anglediff = true_difference(pos_angle(data['COG'].tolist()[index_before]), pos_angle(data['Heading'].tolist()[index_before]))
                 collection.append(anglediff)
-            distance_before = distance_before + distance.distance(coordinate, coordinate_prior).miles
+            distance_before = distance_before + distance.distance(coordinate, coordinate_prior).miles # Uses geopy's distance library to find the distance in miles between datapoints and add it to cumulative 
             print('For index' + str(index_before) + ' on ship' + str(passing.MMSI) + " at time " + data['BaseDateTime'].tolist()[index_before]) # For debugging
             print("The added " + param + " is " + str(collection[ind])) # For debugging
             print("And the cumulative distance is " + str(distance_before) + "\n") # For debugging
-            index_before -= 1
-            ind += 1 # Debugging
+            index_before -= 1 # Index decreases since we are moving "back in time"
+            ind += 1 # For debugging
 
+        # Downstream loop
         index_after = data.index[data['BaseDateTime'] == passing.time_after][0]
         distance_after = 0
         coordinate_prior = (data['LAT'].tolist()[index_after], data['LON'].tolist()[index_after])
-        while distance_after <= 5:
+
+        while distance_after <= 5: # 5 miles downstream 
             coordinate = (data['LAT'].tolist()[index_after], data['LON'].tolist()[index_after])
             coordinate_prior = (data['LAT'].tolist()[index_after - 1], data['LON'].tolist()[index_after - 1])
             if param != 'Angle Difference':    
                 collection.append(data['COG'].tolist()[index_after])
             elif param == 'Angle Difference':
+                # Calculates angle difference according to rules set out in true_difference() function definition
                 anglediff = true_difference(pos_angle(data['COG'].tolist()[index_after]), pos_angle(data['Heading'].tolist()[index_after]))
                 collection.append(anglediff)
-            distance_after = distance_after + distance.distance(coordinate, coordinate_prior).miles
+            distance_after = distance_after + distance.distance(coordinate, coordinate_prior).miles # Uses geopy's distance library to find the distance in miles between datapoints and add it to cumulative 
             print('For index ' + str(index_after) + ' on ship ' + str(passing.MMSI) + " at time " + data['BaseDateTime'].tolist()[index_after]) # For debugging
             print("The added " + param + " is " + str(collection[ind])) # For debugging
             print("And the cumulative distance is " + str(distance_after) + "\n") # For debugging
-            index_after += 1
-            ind += 1 # Debugging
+            index_after += 1 # Index increases since we are now moving forward in time
+            ind += 1 # For debugging
+            
         print(str(passing.Index + 1) + "/" + str(len(bridge_df)) + " through the pass data.") # For debugging
     
     return collection
